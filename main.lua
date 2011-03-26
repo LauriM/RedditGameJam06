af97837d3cdc35bd99a101c5b59a2c89f44a75ec
Lauri <lauri.sakari.makinen@gmail.com>
2011-03-26 08:11:39 +0200

    Lots of changes! Tilemap working with box2d stuff

--- main.lua
+++ main.lua
@@ -1,4 +1,12 @@
 function love.load()
+	world = love.physics.newWorld(-800,-600, 800,600)
+	world:setGravity(0,50)
+
+	bodies = {}
+	shapes = {}
+	alive  = {}
+	info   = {}
+
 	camera_x    = 0
 	camera_y    = 0
 	player_x    = 50
@@ -53,38 +61,73 @@ function love.load()
 	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1},
 	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 	   { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 	   { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 	}
+	
+	count = 0
+	for x=1, map_width do
+		for y=1, map_height do
+			if map_foreground[x][y] > 0 then
+				bodies[count] = love.physics.newBody(world,x * tile_size_x,y * tile_size_y,0,0)
+				shapes[count] = love.physics.newRectangleShape(bodies[count],0, 0,20,20,0)
+				alive[count]  = true
+				info[count]   = 0
+				count = count + 1
+			end
+		end
+	end
 
+	for i=0,250 do
+		alive[i]  = true
+		bodies[i] = love.physics.newBody(world,math.random(25,300),math.random(25,300),20,20)
+		shapes[i] = love.physics.newCircleShape(bodies[i],0,0,5)
+		info[i]   = 1
+	end
 end
 
-function love.update()
+function love.update(dt)
+	world:update(dt)
 end
 
 function love.draw()
 	map_draw()
+	draw_physics()
 	player_draw()
 	love.graphics.print("Hello Wolrd",20,20)
+end
+
+function draw_physics()
+	for i=0,500 do
+		if alive[i] == true then
+			if info[i] == 0 then
+				--no need to render as tiles are drawn using images
+			end
+
+			if info[i] == 1 then
+				love.graphics.circle("fill", bodies[i]:getX(), bodies[i]:getY(),5)
+			end
+		end
+	end
 end
 
 function map_draw()
 	for x=1, map_width do
 		for y=1, map_height do
-			love.graphics.draw(tile[map_background[x][y]], (x * tile_size_x) + camera_x, (y * tile_size_y) + camera_y)
+				love.graphics.draw(tile[map_background[x][y]], (x * tile_size_x) + camera_x - 10, (y * tile_size_y) + camera_y - 10)
 		end
 	end
 
 	for x=1, map_width do
 		for y=1, map_height do
 			if map_foreground[x][y] > 0 then
-				love.graphics.draw(tile[map_foreground[x][y]], (x * tile_size_x) + camera_x, (y * tile_size_y) + camera_y)
+				love.graphics.draw(tile[map_foreground[x][y]], (x * tile_size_x) + camera_x - 10, (y * tile_size_y) + camera_y - 10)
 			end
 		end
 	end
